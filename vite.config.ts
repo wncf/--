07cdc74113b3f3ed73ad9388a22eb54e9e87380a
wncf <2630473215@qq.com>
2023-03-05 10:39:25 +0800

    add

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import { name } from './package.json'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
+import path from 'path'
 import { VantResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,6 +24,18 @@ export default defineConfig({
       less: {
         javascriptEnabled: true,
       },
+    },
+  },
+  experimental: {
+    renderBuiltUrl(
+      filename: string,
+      { hostId, hostType, type }: { hostId: string; hostType: 'js' | 'css' | 'html'; type: 'public' | 'asset' },
+    ) {
+      if (type === 'public') {
+        return `${name}/${filename}`
+      } else {
+        return { relative: true }
+      }
     },
   },
 })
